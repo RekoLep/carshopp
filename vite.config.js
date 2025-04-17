34d0c50d65343174e13dc72968a4f33621d0c72e
RekoLep <159913203+RekoLep@users.noreply.github.com>
2025-04-17 12:56:13 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',  // 👈 tärkeää!
+    emptyOutDir: true, // (vaihtoehtoinen, mutta hyvä että tyhjentää vanhat)
+  }
 })
